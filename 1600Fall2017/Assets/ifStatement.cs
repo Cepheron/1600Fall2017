905e6eb03217598dbe9fdb8b3634b4b5e8918210
Unknown <wolfyschool@gmail.com>
2017-09-11 11:52:28 -0600

    Added if statements

---
+++ 1600Fall2017/Assets/ifStatement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ifStatement : MonoBehaviour
+{
+    public Text input;
+    public string password = "G@m3Pl@y";
+
+    void Update()
+    {
+        if (input.text == password)
+        {
+            print("You know the password!");
+        }
+    }
+}
