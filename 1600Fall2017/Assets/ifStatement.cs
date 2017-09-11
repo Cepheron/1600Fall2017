29f211f5c57b71861383d751b0c0b75207d7f17c
Unknown <wolfyschool@gmail.com>
2017-09-11 12:00:44 -0600

    fixed some things

--- 1600Fall2017/Assets/ifStatement.cs
+++ 1600Fall2017/Assets/ifStatement.cs
@@ -3,14 +3,29 @@ using UnityEngine.UI;
 
 public class ifStatement : MonoBehaviour
 {
-    public Text input;
-    public string password = "G@m3Pl@y";
 
+    public int num1;
+    public int num2;
+    public int value;
+
+    public bool canPlay;
+
+    public Text input;
+    private string password = "G@m3Pl@y";
+    // Use this for initialization
     void Update()
     {
-        if (input.text == password)
+        if (num1 + num2 == value)
         {
-            print("You know the password!");
+            print(value);
+        }
+        if (canPlay)
+        {
+            print("Play");
+        }
+        if(input.text == password)
+        {
+            print("You know the password");
         }
     }
 }
