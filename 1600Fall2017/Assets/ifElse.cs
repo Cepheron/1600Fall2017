8f4b4ba949de76fb030c2eec832010c0515f19b5
Unknown <wolfyschool@gmail.com>
2017-09-13 11:40:39 -0600

    Added if/else and math operators

---
+++ 1600Fall2017/Assets/ifElse.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ifElse : MonoBehaviour
+{
+    public Text input;
+    public bool canPlayGame = false;
+    public string password = "G@m3Pl@y";
+    // Use this for initialization
+    void Update()
+    {
+        if (input.text == password)
+        {
+            print("You know the password");
+        }
+        else
+        { 
+            print("The password is incorrect");
+        }
+        if (canPlayGame)
+        {
+            print("Playing game");
+        }
+        else
+        {
+            print("Can't play yet, enter a correct password");
+        }
+    }
+}
