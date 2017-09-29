856dd6a73475a3d29b1b283cdc26402c70d976ad
Unknown <wolfyschool@gmail.com>
2017-09-29 11:44:50 -0600

    Added Level0 Scene

--- 1600Fall2017/Assets/Scripts/Lists.cs
+++ 1600Fall2017/Assets/Scripts/Lists.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Lists : MonoBehaviour
 {
     public List<GameObject> myWeapons;
-           
+
     void OnTriggerEnter(Collider _weapon)
     {
         myWeapons.Add(_weapon.gameObject);
     }
+
 }
