f270b1f1066af76d106711bd4c4a152fbfb0a745
Unknown <wolfyschool@gmail.com>
2017-10-09 11:32:54 -0600

    Added function with parameters

--- 1600Fall2017/Assets/Animals2.cs
+++ 1600Fall2017/Assets/Animals2.cs
@@ -1,45 +1,14 @@
-<<<<<<< HEAD
 ﻿﻿using System.Collections; 
 using System.Collections.Generic; 
-using UnityEngine; 
- 
-public class Animals2 : MonoBehaviour 
-{ 
-    public string[] foxFood = {"Bananas", "Apples", "Grapes", "Chicken", "Turkey", "Cake"}; 
-    public int[] foodCount = { 10, 2, 30, 1, 2, 1}; 
-     
-    public virtual void Start() 
-    {               
-        for (int i = 0; i < foxFood.Length; i++) 
-        { 
-            Eat(foxFood[i], foodCount[i]); 
-        } 
- 
-        Sleep(); 
-    } 
-        
-    void Eat(string food, int amount) 
-    { 
-        print(this.name + " likes to eat " + food); 
-        print(this.name + " ate " + amount); 
-    } 
- 
-    void Sleep() 
-    { 
-        print(this.name + " Sleeps"); 
-    } 
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Animals2 : MonoBehaviour
 {
-    public string[] foxFood = {"Bananas", "Apples", "Grapes", "Chicken", "Turkey", "Cake"};
-    public int[] foodCount = { 10, 2, 30, 1, 2, 1};
-    
+    public string[] foxFood = { "Bananas", "Apples", "Grapes", "Chicken", "Turkey", "Cake" };
+    public int[] foodCount = { 10, 2, 30, 1, 2, 1 };
+
     public virtual void Start()
-    {              
+    {
         for (int i = 0; i < foxFood.Length; i++)
         {
             Eat(foxFood[i], foodCount[i]);
@@ -47,7 +16,7 @@ public class Animals2 : MonoBehaviour
 
         Sleep();
     }
-       
+
     void Eat(string food, int amount)
     {
         print(this.name + " likes to eat " + food);
@@ -58,5 +27,4 @@ public class Animals2 : MonoBehaviour
     {
         print(this.name + " Sleeps");
     }
->>>>>>> master
 }
