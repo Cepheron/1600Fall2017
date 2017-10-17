db8358fa965c2a147b119e89b1c7e0865f1a27ff
Unknown <wolfyschool@gmail.com>
2017-10-17 17:03:42 -0600

    Made Roll a Ball game

--- Unity Tutorials/Roll a Ball/Roll a Ball/Assets/Scripts/Rotator.cs
+++ Unity Tutorials/Roll a Ball/Roll a Ball/Assets/Scripts/Rotator.cs
@@ -2,15 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotater : MonoBehaviour {
+public class Rotator : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
 	}
 }
