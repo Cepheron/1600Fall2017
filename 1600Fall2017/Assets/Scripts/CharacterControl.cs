33121d68e4bf42ee7590fecbbddd0e8cf7d8bb06
Unknown <wolfyschool@gmail.com>
2017-10-30 12:04:18 -0600

    Added game over and checkpoint

--- 1600Fall2017/Assets/Scripts/CharacterControl.cs
+++ 1600Fall2017/Assets/Scripts/CharacterControl.cs
@@ -2,24 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterControl : MonoBehaviour {
+public class CharacterControl : MonoBehaviour
+{
 
     public CharacterController characterController;
+    public static bool gameOver = false;
     public float gravity = 9.81f;
     public float speed = 10;
     public Vector3 moveVector3;
-    public float jumpForce = 50;
-			
-	void FixedUpdate () {
+    public float jumpForce = 20;
+    void Update()
+    {
         moveVector3.y -= gravity * Time.deltaTime;
-        if (characterController.isGrounded)
+        if (characterController.isGrounded && !gameOver)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 moveVector3.y = jumpForce * Time.deltaTime;
-            }            
+            }
             moveVector3.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        }        
+        }
         characterController.Move(moveVector3);
-	}
+    }
 }
